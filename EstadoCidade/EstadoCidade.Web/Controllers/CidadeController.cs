087ec02da257eac5ab0cc7a30da81eb711766ec9
Waldyr Felix <waldyrfelix@gmail.com>
2013-07-14 22:56:35 -0300

    crud de cidade terminado

---
+++ EstadoCidade/EstadoCidade.Web/Controllers/CidadeController.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using EstadoCidade.Dominio.Intefaces;
+using EstadoCidade.Web.Models;
+
+namespace EstadoCidade.Web.Controllers
+{
+    public class CidadeController : Controller
+    {
+        private readonly IRepositorioDeCidades _repositorioDeCidades;
+        private readonly IRepositorioDeEstados _repositorioDeEstados;
+
+        public CidadeController(IRepositorioDeCidades repositorioDeCidades, IRepositorioDeEstados repositorioDeEstados)
+        {
+            _repositorioDeCidades = repositorioDeCidades;
+            _repositorioDeEstados = repositorioDeEstados;
+        }
+
+        public ActionResult Index()
+        {
+            var cidades = _repositorioDeCidades.Todos().Select(ControllerHelper.TransformarParaViewModel);
+            return View(cidades);
+        }
+
+
+        public ActionResult Cadastrar()
+        {
+            ViewBag.Estados = _repositorioDeEstados.Todos();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(CidadeViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cidade = viewModel.Model();
+                _repositorioDeCidades.Inserir(cidade);
+                return RedirectToAction("Index");
+            }
+            
+            ViewBag.Estados = _repositorioDeEstados.Todos();
+            return View(viewModel);
+        }
+
+        public ActionResult Editar(int id)
+        {
+            ViewBag.Estados = _repositorioDeEstados.Todos();
+            var cidade = _repositorioDeCidades.Obter(id);
+            return View(ControllerHelper.TransformarParaViewModel(cidade));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(CidadeViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cidade = viewModel.Model();
+                _repositorioDeCidades.Atualizar(cidade);
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Estados = _repositorioDeEstados.Todos();
+            return View(viewModel);
+        }
+    }
+}
--- EstadoCidade/EstadoCidade.Web/Models/CidadeViewModel.cs
+++ EstadoCidade/EstadoCidade.Web/Models/CidadeViewModel.cs
@@ -1,17 +1,25 @@
-﻿using EstadoCidade.Dominio;
+﻿using System.ComponentModel.DataAnnotations;
+using EstadoCidade.Dominio;
 
 namespace EstadoCidade.Web.Models
 {
     public class CidadeViewModel
     {
         public int Id { get; set; }
+        [Required, StringLength(50)]
         public string Nome { get; set; }
         public bool Capital { get; set; }
+        [Required]
         public int EstadoId { get; set; }
 
         public Cidade Model()
         {
-            return new Cidade(Id, Nome, Capital, EstadoId);
+            if (Id > 0)
+            {
+                return new Cidade(Id, Nome, Capital, EstadoId);
+                
+            }
+            return new Cidade(Nome, Capital, EstadoId);
         }
     }
 }
